ed98512b48c42d942ca45c0539d9f3eca01a0781
NikitaBaranov1992 <2486010@mail.ru>
2023-06-06 00:11:36 +0500

    Homework 17.2.2

--- cypress/e2e/santaAPI.cy.js
+++ cypress/e2e/santaAPI.cy.js
@@ -1,30 +1,21 @@
-describe.only("santa API", () => {
-  beforeEach(() => {
-    cy.request({
-      method: "POST",
-      headers: {},
-      url: "https://santa-secret.ru/login",
-      body: {
-        email: "4932843+1@gmail.com",
-        password: "GoodPassword10",
-      },
-    }).then((response) => {
-      expect(response.status).to.eq(200);
-    });
-  });
+import { faker } from "@faker-js/faker";
+let newName = faker.internet.domainWord(10);
+let newKey = faker.internet.password(10);
+let SantaCookie =
+  "_ym_uid=1685986829782511802; _ym_d=1685990622; _ym_isad=2; jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjQ5NTUxMTAsImlhdCI6MTY4NTk5MDYyNCwiZXhwIjoxNjg4NTgyNjI0fQ.ycPm3LO2KhmFEsW0QOPvHCuOSWF_r1o_dZGA1phW1H4; adtech_uid=970c451e-92d2-4560-937b-a90a748bb431%3Asanta-secret.ru; top100_id=t1.7627570.682147776.1685990629710; _ohmybid_cmf=2; last_visit=1685973052478%3A%3A1685991052478; t3_sid_7627570=s1.138401598.1685990629711.1685991055671.1.9";
 
+describe("santa API", () => {
   it("Add box", () => {
     cy.request({
       method: "POST",
       headers: {
-        /* Cookie:
-          "_ym_uid=1685035036523428104; _ym_d=1685035036; __upin=tTskgndEB7iuwZSlTT9v/Q; adtech_uid=a993c893-f711-4e44-a182-801a4bba29b2%3Asanta-secret.ru; top100_id=t1.7627570.2043786471.1685035102505; _ym_isad=2; jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjQ5NTU1MDYsImlhdCI6MTY4NTYzOTg4NCwiZXhwIjoxNjg4MjMxODg0fQ.JoPaxrODNEsYcekQtksOrIWPH1jP2dAb23RRxBOIKuw; _ohmybid_cmf=2; uuid=8d5ea98c0eb4bfb6%3A4; _buzz_fpc=JTdCJTIycGF0aCUyMiUzQSUyMiUyRiUyMiUyQyUyMmRvbWFpbiUyMiUzQSUyMi5zYW50YS1zZWNyZXQucnUlMjIlMkMlMjJleHBpcmVzJTIyJTNBJTIyU2F0JTJDJTIwMDElMjBKdW4lMjAyMDI0JTIwMTclM0ExOSUzQTU2JTIwR01UJTIyJTJDJTIyU2FtZVNpdGUlMjIlM0ElMjJMYXglMjIlMkMlMjJ2YWx1ZSUyMiUzQSUyMiU3QiU1QyUyMnVmcCU1QyUyMiUzQSU1QyUyMmNiOTljMzNmY2RjZjI2MzhkYThjYzc0YTIzZWMwOWIzJTVDJTIyJTJDJTVDJTIyYnJvd3NlclZlcnNpb24lNUMlMjIlM0ElNUMlMjIxMTMuMCU1QyUyMiU3RCUyMiU3RA==; last_visit=1685622014303%3A%3A1685640014303; t3_sid_7627570=s1.521300221.1685639690203.1685640014504.5.5",*/
+        Cookie: SantaCookie,
       },
       url: "https://santa-secret.ru/api/box",
       body: {
         email: null,
-        name: "{{$randomWord}}",
-        key: "{{$randomWord}}",
+        name: newName,
+        key: newKey,
         picture: "santa",
         usePost: false,
         useCashLimit: null,
@@ -48,14 +39,13 @@ describe.only("santa API", () => {
     cy.request({
       method: "PUT",
       headers: {
-        /* Cookie:
-          "_ym_uid=1685035036523428104; _ym_d=1685035036; __upin=tTskgndEB7iuwZSlTT9v/Q; adtech_uid=a993c893-f711-4e44-a182-801a4bba29b2%3Asanta-secret.ru; top100_id=t1.7627570.2043786471.1685035102505; _ym_isad=2; jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjQ5NTU1MDYsImlhdCI6MTY4NTYzOTg4NCwiZXhwIjoxNjg4MjMxODg0fQ.JoPaxrODNEsYcekQtksOrIWPH1jP2dAb23RRxBOIKuw; _ohmybid_cmf=2; uuid=8d5ea98c0eb4bfb6%3A4; _buzz_fpc=JTdCJTIycGF0aCUyMiUzQSUyMiUyRiUyMiUyQyUyMmRvbWFpbiUyMiUzQSUyMi5zYW50YS1zZWNyZXQucnUlMjIlMkMlMjJleHBpcmVzJTIyJTNBJTIyU2F0JTJDJTIwMDElMjBKdW4lMjAyMDI0JTIwMTclM0ExOSUzQTU2JTIwR01UJTIyJTJDJTIyU2FtZVNpdGUlMjIlM0ElMjJMYXglMjIlMkMlMjJ2YWx1ZSUyMiUzQSUyMiU3QiU1QyUyMnVmcCU1QyUyMiUzQSU1QyUyMmNiOTljMzNmY2RjZjI2MzhkYThjYzc0YTIzZWMwOWIzJTVDJTIyJTJDJTVDJTIyYnJvd3NlclZlcnNpb24lNUMlMjIlM0ElNUMlMjIxMTMuMCU1QyUyMiU3RCUyMiU3RA==; last_visit=1685622014303%3A%3A1685640014303; t3_sid_7627570=s1.521300221.1685639690203.1685640014504.5.5",*/
+        Cookie: SantaCookie,
       },
-      url: "https://santa-secret.ru/api/box",
+      url: "https://santa-secret.ru/api/box" + newKey,
       body: {
         email: null,
-        name: "{{$randomWord}}",
-        key: "{{key}}",
+        name: "newName",
+        key: "",
         picture: "cookie_star",
         usePost: false,
         useCashLimit: false,
@@ -79,10 +69,9 @@ describe.only("santa API", () => {
     cy.request({
       method: "DELETE",
       headers: {
-        /* Cookie:
-          "_ym_uid=1685035036523428104; _ym_d=1685035036; __upin=tTskgndEB7iuwZSlTT9v/Q; adtech_uid=a993c893-f711-4e44-a182-801a4bba29b2%3Asanta-secret.ru; top100_id=t1.7627570.2043786471.1685035102505; _ym_isad=2; jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjQ5NTU1MDYsImlhdCI6MTY4NTYzOTg4NCwiZXhwIjoxNjg4MjMxODg0fQ.JoPaxrODNEsYcekQtksOrIWPH1jP2dAb23RRxBOIKuw; _ohmybid_cmf=2; uuid=8d5ea98c0eb4bfb6%3A4; _buzz_fpc=JTdCJTIycGF0aCUyMiUzQSUyMiUyRiUyMiUyQyUyMmRvbWFpbiUyMiUzQSUyMi5zYW50YS1zZWNyZXQucnUlMjIlMkMlMjJleHBpcmVzJTIyJTNBJTIyU2F0JTJDJTIwMDElMjBKdW4lMjAyMDI0JTIwMTclM0ExOSUzQTU2JTIwR01UJTIyJTJDJTIyU2FtZVNpdGUlMjIlM0ElMjJMYXglMjIlMkMlMjJ2YWx1ZSUyMiUzQSUyMiU3QiU1QyUyMnVmcCU1QyUyMiUzQSU1QyUyMmNiOTljMzNmY2RjZjI2MzhkYThjYzc0YTIzZWMwOWIzJTVDJTIyJTJDJTVDJTIyYnJvd3NlclZlcnNpb24lNUMlMjIlM0ElNUMlMjIxMTMuMCU1QyUyMiU3RCUyMiU3RA==; last_visit=1685622014303%3A%3A1685640014303; t3_sid_7627570=s1.521300221.1685639690203.1685640014504.5.5",*/
+        Cookie: SantaCookie,
       },
-      url: "https://santa-secret.ru/api/box/{{key}}",
+      url: "https://santa-secret.ru/api/box/" + newKey,
     }).then((response) => {
       expect(response.status).to.eq(200);
     });
